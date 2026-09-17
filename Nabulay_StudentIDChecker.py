f77dde341833985a99429f498a063fdaa91fcfd0
Gabriel Seth B. Nabulay <gbnabulay@carc.pshs.edu.ph>
2026-09-17 16:49:14 +0800

    Add files via upload

--- Nabulay_StudentIDChecker.py
+++ Nabulay_StudentIDChecker.py
@@ -1,12 +1,13 @@
 # Name: Nabulay, Gabriel Seth B.
 # Grade and Section: 8-Adelfa
 
-import re
+import re # Imports RegEx
 
 while True:
-    student_ID = input("\nEnter Student ID: ")
+    student_ID = input("\nEnter Student ID: ") # Inputs user's student ID
 
-    if re.fullmatch(r"\d{4}-\d{4}", student_ID):
-        print("Valid Student ID.")
+    if re.fullmatch(r"\d{4}-\d{4}", student_ID): # Checks if student ID's pattern follows this pattern (4 digits - 4 digits)
+        print("Valid Student ID.") # Outputs this message if student ID pattern is valid.
+        break
     else:
-        print("Invalid Student ID.")
+        print("Invalid Student ID.") # Outputs this message if student ID pattern is invalid and makes user re-input
